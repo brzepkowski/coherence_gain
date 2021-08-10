771d7109881940848cb799b28560e3e19c9268eb
brzepkowski <incidental.address@gmail.com>
2021-08-10 11:07:11 +0200

    Added scripts which enable to look closely into Weyl's operators

--- run_job.sh
+++ run_job.sh
@@ -11,11 +11,11 @@ PBS="#!/bin/bash\n\
 #PBS -N ${NAME}\n\
 #PBS -l walltime=2:00:00\n\
 #PBS -l select=1:ncpus=1:mem=256MB\n\
-#PBS -l software=generate_data_1D_t_finite.py\n\
+#PBS -l software=generate_data_1D_t_finite_detailed.py\n\
 #PBS -m n\n\
 cd \$PBS_O_WORKDIR\n\
 
-python3 generate_data_1D_t_finite.py ${T} ${t} ${tau_min} ${tau_max}"
+python3 generate_data_1D_t_finite_detailed.py ${T} ${t} ${tau_min} ${tau_max}"
 
 # Echo the string PBS to the function qsub, which submits it as a cluster job for you
 # A small delay is included to avoid overloading the submission process
