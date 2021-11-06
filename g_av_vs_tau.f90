44d7377c473bcc61242f59fde947601dc73eb031
brzepkowski <incidental.address@gmail.com>
2021-11-06 10:51:44 +0100

    Small fix in finding the minimal value of g_av in given tau's range.

--- g_av_vs_tau.f90
+++ g_av_vs_tau.f90
@@ -55,12 +55,13 @@ program main
         tau_time = tau_time_start + tau_time_step*iterator
         call g_average(t_time,tau_time,T_temp,W_0,W_1,W_1_phased,W_2,W_2_phased,W_3,phase, &
             D_plus,D_minus,D_t_minus_tau,p_plus,p_minus,g_av)
+            
         if (g_av > max_g_av) then
             max_g_av = g_av
             tau_of_g_av_max = tau_time
         end if
 
-        if (g_av < min_g_av .and. g_av > 0.) then
+        if (g_av < min_g_av) then
             min_g_av = g_av
             tau_of_g_av_min = tau_time
         end if
